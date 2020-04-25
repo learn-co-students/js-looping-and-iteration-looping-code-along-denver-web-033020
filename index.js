13b4c3f4222444da218e87bba33ac1cdf0516af6
austinckohler <austinckohler@gmail.com>
2020-04-25 16:01:18 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    let cards = []
+    for (let i = 0; i < names.length; i++) {
+        cards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return cards
+}
+writeCards(names, surpise);
+
+function countDown(beginning) {
+while (beginning > 0) {
+    console.log(beginning);
+    beginning -= 1;
+}
+console.log(beginning);
+}
+
