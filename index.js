cf7e7ba531c51e08c7115ee1c7bd1b77b9437385
wolf-dominion <hannah_drury@yahoo.com>
2020-05-11 15:40:32 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+
+function writeCards(names){
+    let newArray = []
+    for(let i = 0; i < names.length; i++){
+       newArray[i] = `Thank you, ${names[i]}, for the wonderful surprise gift!`
+    }
+    return newArray
+}
+
+function countDown(num){
+    for(num; num >= 0; num--){
+        console.log(num)
+    }
+}
